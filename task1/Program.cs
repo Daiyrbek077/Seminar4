6911f406332d71cb0ee9011a6dc817b38b338ad9
Diyrbek <gulzigitovdajyr@gmail.com>
2024-03-16 08:19:09 +0600

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿while (true) // true == true
+{
+    Console.Write("Введите текст: ");
+    string text = Console.ReadLine();
+    if (text == "q")
+    {
+        break;
+    }
+    // Нужно проверить, что строчка text cостоит ТОЛЬКО
+    // из цифр
+    int number; // 0, если в строчке есть символы
+                // или само число
+    if (int.TryParse(text, out number)) // == true
+    {
+        Console.WriteLine("Введенная строчка состоит из ЦИФР");
+        int sum = 0; // 56 => 6+5
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10; // Избавляюсь от последней цифры
+        }
+        if (sum % 2 == 0)
+        {
+            break;
+        }
+    }
+}
