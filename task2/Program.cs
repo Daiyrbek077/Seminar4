c4e95b6b52a81b9df6099f90a0398e02ae757edf
Diyrbek <gulzigitovdajyr@gmail.com>
2024-03-16 08:53:57 +0600

    add task3

---
+++ task2/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size]; // Массив array на size элементов
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+
+int GetEvenNumbers(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++; // count = count + 1
+        }
+    }
+    return count;
+}
+
+
+// Вызов функции
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] res = CreateArray(N, 100, 999);
+// CreateArray(N, 10, 99) => массив, размер: N
+// каждый элемент: число от 100 до 999 вкл-но
+Console.WriteLine($"Массив: [ {string.Join("; ", res)} ]");
+Console.WriteLine($"Even numbers: {GetEvenNumbers(res)}");
---
+++ task3/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size]; // Массив array на size элементов
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+// Перевернить массив:
+int[] GetReverseReading(int[] arr)
+{
+    int[] array = new int[arr.Length];
+    for (int i = 0, j = arr.Length-1; i < arr.Length; i++, j--)
+    {
+        array[i] = arr[j];
+    }
+    return array;
+}
+
+// Получить массив случайными числами от 0 до 100:
+int[] result = CreateArray(10, 0, 100);
+
+// перевернить массив
+int[] revers = GetReverseReading(result);
+
+
+Console.WriteLine($"Массив ДО: [ {string.Join(", ", result)} ]  => ");
+Console.WriteLine($"Массив ПОСЛЕ: [ {string.Join(", ", revers)} ]");
